510d5b57cd08d1395969ab8be81c03e17c9675a2
diplamtuan <85506794+diplamtuan@users.noreply.github.com>
2022-09-26 23:44:59 +0700

    update

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+const leftBtn = $('.container-slider__control-left');
+const rightBtn = $('.container-slider__control-right');
+const dots = $$('.container-slider__control-dot');
+const img = $$('.container-slider__img');
+
+var slideIndex = 0;
+
+showDivs(slideIndex);
+leftBtn.onclick = function () {
+    slideIndex += -1;
+    showDivs(slideIndex);
+}
+
+rightBtn.onclick = function () {
+    slideIndex += 1;
+    showDivs(slideIndex);
+}
+
+function currentDiv(n) {
+    showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+    var i;
+    if (n < 0) slideIndex = img.length - 1;
+    if (n > img.length - 1) slideIndex = 0;
+    for (i = 0; i < img.length; i++) {
+        img[i].style.display = 'none';
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].classList.remove('container-slider__control-dot--active');
+    }
+    img[slideIndex].style.display = 'block';
+    dots[slideIndex].classList.add('container-slider__control-dot--active');
+}
+
